f00238e7481371984e45946705c876e7af9a1af4
John Paul Banera <impaulintech@gmail.com>
2023-09-07 01:43:46 +0800

    [kltn-05]-Landing and Transcribe page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,47 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
+import KapampanganFont from "@/shared/images/kapampangan-font.png";
+
+import Card from "@/components/templates/Card";
+import { Kapampangan } from "@/shared/icons/Kapampangan";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen min-w-screen flex- items-start justify-start p-12">
-			<div className="header w-full">
+		<main className="min-h-screen min-w-screen flex flex-col items-start justify-start p-12 gap-12">
+			<div className="w-full">
 				<div className="flex-col flex-wrap max-w-[120px]">
-					<p className="text-xl font-medium shadow-font">KU LIT AN</p>
-					<p className="text-xl font-medium font-kulitan flex gap-2 items-center justify-between -mt-2 max-w-[96px]">
+					<p className="text-xl font-medium text-shadow">KU LIT AN</p>
+					<p className="text-xl font-medium text-shadow font-kulitan flex gap-2 items-center justify-between -mt-2 max-w-[96px]">
 						<span>KU</span>
 						<span>LIT</span>
 						<span>AN</span>
 					</p>
 				</div>
 			</div>
+			<div className="w-full flex flex-col gap-5">
+				<Card
+					link="transcribe"
+					buttonTitle={
+						<>
+							Transcribe | <span className="font-kulitan">i lu lin</span>
+						</>
+					}
+				>
+					<Image
+						src={KapampanganFont}
+						alt="kapamapangan font"
+						width={60}
+						height={100}
+					/>
+					<p className="h-[30%] pt-2 text-shadow">kapampangan</p>
+				</Card>
+			</div>
+			<footer className="absolute bottom-10 left-0 w-full flex items-center justify-center">
+				<span className="font-kulitan text-dark opacity-50 font-bold text-[18px]">
+					ka pang pang an
+				</span>
+			</footer>
 		</main>
 	);
 }
--- src/app/transcribe/page.tsx
+++ src/app/transcribe/page.tsx
@@ -1,26 +1,72 @@
 "use client";
-import { useState } from 'react';
-import kulitanizeWords from '@/utils/kulitanizeWords';
+import { useRef, useState } from "react";
+import kulitanizeWords from "@/utils/kulitanizeWords";
+import { BackArrow } from "@/shared/icons/BackArrow";
+import TransparentCard from "@/components/templates/TransparentCard";
 
 export default function Transcribe() {
-  const [inputValue, setInputValue] = useState('agagagganggaanga');
+	const editableContainerRef = useRef<HTMLDivElement | null>(null);
 
-  const handleChange = (e:any) => {
-    setInputValue(e.target.value);
-  };
+	const onBeforeInput = (e: any) => {
+		if (e.inputType === "insertLineBreak" && e.shiftKey) {
+			e.preventDefault();
+			const currentDiv: any = document.activeElement;
+			currentDiv.innerText += "\n";
 
-  const result = kulitanizeWords(inputValue);
+			// Move the cursor to the end of the last innerText
+			const range = document.createRange();
+			const selection: any = window.getSelection();
+			range.selectNodeContents(currentDiv);
+			range.collapse(false); // Move to the end
+			selection.removeAllRanges();
+			selection.addRange(range);
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24 bg-gradient-container">
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleChange}
-        placeholder="Enter text"
-        className='text-slate-900'
-      />
-      <p>Result: <span className='font-kulitan'>{result}</span></p>
-    </main>
-  );
+			// Optionally, focus on the element
+			currentDiv.focus();
+		}
+	};
+
+	const onKeydown = (e: any) => {
+		if (e.key === "Enter" && !e.shiftKey) {
+			e.preventDefault();
+			const newDiv: any = document.createElement("div");
+			newDiv.contentEditable = true;
+			newDiv.className = "w-10 text-right outline-0";
+			newDiv.onkeydown = onKeydown;
+			editableContainerRef.current?.appendChild(newDiv);
+			newDiv.focus();
+		}
+	};
+
+	return (
+		<main className="flex min-h-screen min-w-screen flex-col items-center justify-start gap-5 bg-gradient-container">
+			<div className="w-full flex justify-center items-center relative h-[49px]">
+				<div className="bg-black w-full absolute h-full z-0 opacity-30"></div>
+				<BackArrow className="absolute left-5 z-10" />
+				<p className="font-kulitan text-[20px] z-10">i lu lin</p>
+			</div>
+			<TransparentCard>
+				<textarea
+					className="z-30 text-light bg-transparent resize-y w-[350px] min-w-[350px] max-w-[350px] min-h-[96px] outline-0"
+					name="postContent"
+					rows={3}
+					cols={40}
+				/>
+			</TransparentCard>
+			<TransparentCard className="flex justify-end items-end">
+				<div
+					id="editableContainer"
+					ref={editableContainerRef}
+					className="flex flex-row-reverse justify-start items-start w-[350px] gap-1 max-w-[350px] overflow-x-scroll p-3 min-h-[210px] h-[210px] resize-y"
+				>
+					<div
+						contentEditable={true}
+						className="w-10 text-right outline-0"
+						onBeforeInput={onBeforeInput}
+						onKeyDown={onKeydown}
+					></div>
+				</div>
+			</TransparentCard>
+		</main>
+	);
 }
